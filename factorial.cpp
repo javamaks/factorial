61cbbdd3cc34995dcb749cd6aea1e4c3181629ea
javamaks <63841611+javamaks@users.noreply.github.com>
2022-12-23 03:09:16 +0500

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-using namespace std;
-int Factorial(int n){
+//Факториал числа на языке программирования С++ и реализация с использованием функции
+#include <iostream> //Подключение библиотеки
+using namespace std; 
+int Factorial(int n){ //Функция факториала
     int f=1;
     for(int i=1; i<=n; i++){
         f*=i;
      }
-    return f;
+    return f; //Вернуть значение
   }    
 int main(){
-    int n, a;
-    cin >> n;
-    a=Factorial(n);
-    cout<<a;
+    int n, a; //Переменные
+    cin >> n; //Ввод переменной
+    a=Factorial(n); 
+    cout<<a; //Вывод значения
 }
